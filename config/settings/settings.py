e5ac81c25ea13fa8caa28e612465a3a27a4b64c1
Lyte <litecode77@gmail.com>
2025-10-20 18:01:01 -0700

    setting up whitenoise for prod

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -176,6 +176,8 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
